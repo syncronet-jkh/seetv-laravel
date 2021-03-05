59dfa4c3b3964c224bbec47e01ac4c8ebd5361e0
Jonas Hansen <jkh@syncronet.dk>
2021-03-05 14:06:00 +0100

    move CSRF token to web routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\CSRFTokenController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/api/csrf-token', [CSRFTokenController::class, 'index']);
 Route::view('/', 'welcome');
